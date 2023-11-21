c17c434e827c7950da4d5959eb6350844d56ee81
Sunny-jinn <rlawlsdn316@gmail.com>
2023-11-21 20:13:20 +0900

    feat: 파일 업로드 api 연동

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -1,19 +1,42 @@
 import { ChangeEvent, useState } from "react";
-import { FileDiv, PreviewImg, UploadLabel } from "./styled";
+import { FileDiv, PreviewImg, SendButton, UploadLabel } from "./styled";
 
 const FileUpload = () => {
-  const [file, setFile] = useState<File | null>();
+  const [file, setFile] = useState<FileList | null>();
   const [fileExtension, setFileExtension] = useState<string | null>();
 
   const fileUploadHandle = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     if (e.target.files) {
-      setFile(e.target.files[0]);
+      setFile(e.target.files);
 
       const fileNameParts = e.target.files[0].name.split(".");
       if (fileNameParts.length > 1) {
         const extension = fileNameParts.pop()!.toLowerCase(); // 소문자로 변환
         setFileExtension(extension);
+      }
+    }
+  };
+
+  const clickHandle = async () => {
+    if (file) {
+      const formData = new FormData();
+      for (let i = 0; i < file.length; i++) {
+        formData.append("files", file[i]);
+      }
+
+      try {
+        const res = await fetch("http://localhost:8000/conv/upload/", {
+          method: "POST",
+          body: formData,
+        });
+        if (res.ok) {
+          console.log("업로드 성공");
+        } else {
+          console.log("업로드 실패");
+        }
+      } catch (err) {
+        console.log(err);
       }
     }
   };
@@ -33,7 +56,7 @@ const FileUpload = () => {
               src={`/assets/${fileExtension === "pdf" ? "pdf" : "docx"}.png`}
               alt="파일"
             />
-            {file.name}
+            {file[0].name}
           </>
         )}
       </div>
@@ -46,6 +69,7 @@ const FileUpload = () => {
         accept=".pdf, .docx, .doc"
       />
       <UploadLabel htmlFor="file">파일 선택</UploadLabel>
+      <SendButton onClick={clickHandle}>업로드</SendButton>
     </FileDiv>
   );
 };
--- src/components/FileUpload/styled.ts
+++ src/components/FileUpload/styled.ts
@@ -4,7 +4,9 @@ export const FileDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   width: 280px;
+  height: 700px;
   border-right: 2px solid #e0e0e0;
 `;
 
@@ -20,6 +22,29 @@ export const UploadLabel = styled.label`
   font-weight: 600;
   cursor: pointer;
   transition: 0.3s;
+
+  &:hover {
+    background: #354fcf; // 어두운 색상으로 변경
+  }
+
+  &:active {
+    background: #17225b; // 어두운 색상으로 변경
+  }
+`;
+
+export const SendButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #3b58e6;
+  width: 200px;
+  height: 40px;
+  color: #fff;
+  border-radius: 5px;
+  font-weight: 600;
+  cursor: pointer;
+  transition: 0.3s;
+  margin-top: 20px;
 
   &:hover {
     background: #354fcf; // 어두운 색상으로 변경

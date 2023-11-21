4cb6e49381b2147ff25b13be57a0aab446657035
Sunny-jinn <rlawlsdn316@gmail.com>
2023-11-21 23:44:50 +0900

    feat: 다크모드 버튼 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,25 @@
-import { Container, Wrapper } from "./styled";
+import { Container, Wrapper, DarkModeIcon } from "./styled";
+import { DarkMode } from "@emotion-icons/material-outlined/DarkMode";
+import { LightMode } from "@emotion-icons/material/LightMode";
+import { useState } from "react";
 
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  const clickHandle = () => {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  };
+
   return (
     <Container>
-      <Wrapper>뭐든지 물어봐!</Wrapper>
+      <Wrapper>
+        <div>뭐든지 물어봐!</div>
+        <DarkModeIcon onClick={clickHandle}>
+          {isDarkMode ? <DarkMode width={30} /> : <LightMode width={30} />}
+
+          {/* <LightMode width={30} /> */}
+        </DarkModeIcon>
+      </Wrapper>
     </Container>
   );
 };
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -13,3 +13,18 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
 `;
+
+export const DarkModeIcon = styled.div`
+  margin-left: auto;
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover {
+    background: #eeeeee;
+  }
+`;
